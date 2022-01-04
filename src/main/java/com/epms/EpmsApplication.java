e20b79405f9f224c6d6ade897a512a1913b84a7e
Shubham Patel <shubhampatel9395@gmail.com>
2022-01-04 15:29:43 +0530

    Initial Commit

---
+++ src/main/java/com/epms/EpmsApplication.java
@@ -0,0 +1,13 @@
+package com.epms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmsApplication.class, args);
+	}
+
+}
