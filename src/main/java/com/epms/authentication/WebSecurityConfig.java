0134534f19d4413d272127a17ded9056431409aa
shubhampatel <shubhampatel9395@gmail.com>
2022-02-23 16:54:38 +0530

    [logout code chaneg]

--- src/main/java/com/epms/authentication/WebSecurityConfig.java
+++ src/main/java/com/epms/authentication/WebSecurityConfig.java
@@ -1,5 +1,11 @@
 package com.epms.authentication;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +14,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    @Autowired 
 	    private LoginSuccessHandler loginSuccessHandler;
@@ -54,11 +65,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	                .successHandler(loginSuccessHandler)
 	                .and()
 	                .logout()
-	                .logoutUrl("/logout")
-	                .invalidateHttpSession(true)
-	                .permitAll()
-	            .and()
-	            .logout().logoutSuccessUrl("/").permitAll();
+	                .logoutSuccessHandler(new SimpleUrlLogoutSuccessHandler() {
+	                    
+	                    @Override
+	                    public void onLogoutSuccess(HttpServletRequest request,
+	                                HttpServletResponse response, Authentication authentication)
+	                            throws IOException, ServletException {
+	                         
+	                        CustomUserDetailsDTO userDetails = (CustomUserDetailsDTO) authentication.getPrincipal();
+	                        String username = userDetails.getUsername();
+	                         
+	                        log.info("The user " + username + " has logged out.");
+	                         
+	                        super.onLogoutSuccess(request, response, authentication);
+	                    }
+	                })
+	            .permitAll();
 	    }
 	     
 	 
